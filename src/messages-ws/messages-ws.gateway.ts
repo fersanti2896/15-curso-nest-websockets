274a2d1217a034524441962e8066c42cb0b0aa2e
Fernando Nicolás <fersanti2896@gmail.com>
2023-09-09 13:42:59 -0600

    Configurando server de socket

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,16 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(private readonly messagesWsService: MessagesWsService) {}
+ 
+  handleDisconnect( client: Socket ) {
+    console.log('Cliente conectado', client.id)
+  }
+ 
+  handleConnection( client: Socket ) {
+    console.log('Cliente desconectado', client.id)
+  }
 }
