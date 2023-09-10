518079090b206ad2a1fd02ac30c0768060852b4a
Fernando Nicolás <fersanti2896@gmail.com>
2023-09-09 22:32:20 -0600

    Definiendo configuraciones de server, emitiendo.

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,16 +1,42 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
+import { NewMessageDto } from './dtos/new-message.dto';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() ws: Server;
+
   constructor(private readonly messagesWsService: MessagesWsService) {}
  
-  handleDisconnect( client: Socket ) {
-    console.log('Cliente conectado', client.id)
-  }
- 
   handleConnection( client: Socket ) {
-    console.log('Cliente desconectado', client.id)
+    this.messagesWsService.registerClient( client );
+    this.ws.emit( 'clients-updated', this.messagesWsService.getConnectedClients() )
+  }
+
+  handleDisconnect( client: Socket ) {
+    this.messagesWsService.removeClient( client.id );
+    this.ws.emit( 'clients-updated', this.messagesWsService.getConnectedClients() )
+  }
+
+  @SubscribeMessage('message-from-client')
+  async handleMessageFromClient( client: Socket, payload: NewMessageDto ) {
+    /* Emite únicamente al cliente */
+    // client.emit('message-from-server', {
+    //   fullName: 'Soy yo!',
+    //   message: payload.message || 'no message'
+    // });
+
+    /* Emite a todos menos al cliente inicial */
+    // client.broadcast.emit('message-from-server', {
+    //   fullName: 'Soy yo!',
+    //   message: payload.message || 'no message'
+    // });
+
+    /* Emite a todos */
+    this.ws.emit('message-from-server', {
+      fullName: 'Soy yo!',
+      message: payload.message || 'no message'
+    });
   }
 }
